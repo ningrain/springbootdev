b535c71187ba3dfa9ba60195f26a185de0d40ffa
ningrain <602294071@qq.com>
2018-07-05 22:35:30 +0800

    sql

--- src/main/resources/sql/一维并二维数组拆分.sql
+++ src/main/resources/sql/一维并二维数组拆分.sql
@@ -1,8 +1,20 @@
 SELECT
   e,
   ordinality,
-  unnest ( b [ ordinality : ordinality ] ) ,
+  unnest(b [ordinality :ordinality]),
   c
 FROM
   tbl_array,
-  unnest ( a ) WITH ORDINALITY e
+  unnest ( a ) WITH ORDINALITY e;
+
+select *
+from (
+       SELECT
+         e,
+         unnest(b [ordinality :ordinality]) as be,
+         c
+       FROM
+         tbl_array,
+         unnest ( a ) WITH ORDINALITY e
+     ) t
+where be is not null;
