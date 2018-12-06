d991a54aa7391ff32024b0f29741b0a3ef94788c
姜宁宁 <jiangningning@haohandata.com.cn>
2018-12-06 16:58:54 +0800

    加载外部扩展配置文件

--- src/main/java/com/hh/springbootdev/SpringbootdevApplication.java
+++ src/main/java/com/hh/springbootdev/SpringbootdevApplication.java
@@ -9,6 +9,19 @@ public class SpringbootdevApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication();
         springApplication.setAddCommandLineProperties(false);
+        configSetting();
         SpringApplication.run(SpringbootdevApplication.class, args);
+    }
+
+    private static void configSetting(){
+        String cfgPath = System.getProperty("cfgPath");
+        if (cfgPath == null){
+            if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                cfgPath = "F:/application.properties";
+            } else {
+                cfgPath = "/root/application.properties";
+            }
+            System.setProperty("cfgPath", cfgPath);
+        }
     }
 }
---
+++ src/main/java/com/hh/springbootdev/configuration/LocalEnvironmentPostProcessor.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2008-2015 浩瀚深度 All Rights Reserved.
+ *
+ * <p>FileName: LocalEnvironmentPostProcessor.java</p>
+ *
+ * @author jiangningning
+ * @date 2018/12/5
+ * @version 1.0
+ * History:
+ * v1.0.0, 姜宁宁 2018/12/5 Create
+ */
+package com.hh.springbootdev.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * <p>Title: LocalEnvironmentPostProcessor</p>
+ * <p>Description: 配置文件扩展</p>
+ *
+ * @author jiangningning
+ */
+public class LocalEnvironmentPostProcessor implements EnvironmentPostProcessor {
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        String filePath = System.getProperty("cfgPath");
+        System.out.println("*********加载外部扩展文件：" + filePath + "**********");
+        try {
+            InputStream is = new FileInputStream(filePath);
+            Properties properties = new Properties();
+            properties.load(is);
+            MutablePropertySources propertySource = environment.getPropertySources();
+            propertySource.addFirst(new PropertiesPropertySource("localconfig", properties));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
